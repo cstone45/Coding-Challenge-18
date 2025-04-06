993239b5339e6da2cf8f4179c843563a4bb90634
cstone45 <cstone45@usf.edu>
2025-04-06 19:46:19 -0400

    Task 2: Import Greeting to App.jsx

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-function Greeting({}) {
+function Greeting() {
     <div>
         <h2>Hello from the Greeting Component!</h2>
         <p>This is your first custom component in React.</p>
     </div>
 }
+return Greeting();
 export default Greeting;
