87c0c1cd065b79e28e4c5f8538763383df431509
cstone45 <cstone45@usf.edu>
2025-04-06 17:18:33 -0400

    Task 2: Created a New Greeting Component

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+function Greeting({}) {
+    <div>
+        <h2>Hello from the Greeting Component!</h2>
+        <p>This is your first custom component in React.</p>
+    </div>
+}
+export default Greeting;
