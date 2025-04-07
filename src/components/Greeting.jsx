8045cd93dd28dce64f9e530d39d9a0b2747b2b0b
cstone45 <cstone45@usf.edu>
2025-04-06 20:01:23 -0400

    Removed Return command which generated an error

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -4,6 +4,5 @@ function Greeting() {
         <h2>Hello from the Greeting Component!</h2>
         <p>This is your first custom component in React.</p>
     </div>
-}
-return Greeting();
+};
 export default Greeting;
